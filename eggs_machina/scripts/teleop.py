e1f84d1fb3f5ce535495cfa7613b066b0301d4f5
Alan Bohannon <alanbohannon@gmail.com>
2024-09-20 09:42:24 -0700

    manage robots

--- eggs_machina/scripts/teleop.py
+++ eggs_machina/scripts/teleop.py
@@ -1,16 +1,39 @@
 import os
 import sys
+from typing import Any, List
+
+
+def instantiate_robots() -> List[Any]:
+    """Define and instantiate all robots used during teleop."""
+    return []
+
+
+def start_teleop():
+    """Trigger teleop when user input is entered."""
+    pass
+
+
+def teleop():
+    """Start tele-operation."""
+    pass
 
 
 def main():
     pass
 
 
+def shutdown_robots_gracefully(robots: List[Any]):
+    """Gracefully turn off all robots."""
+    pass
+
+
 if __name__ == "__main__":
+    robots = instantiate_robots()
     try:
         main()
     except KeyboardInterrupt:
         print("Shutdown requested...exiting")
+        shutdown_robots_gracefully(robots)
         try:
             sys.exit(130)
         except SystemExit:
