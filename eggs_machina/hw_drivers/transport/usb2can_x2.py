5aa726a3d830edcb8231a8f78c3cd6b09ca9d94a
Alan Bohannon <alanbohannon@gmail.com>
2024-09-26 18:21:35 -0700

    create recv function

--- eggs_machina/hw_drivers/transport/usb2can_x2.py
+++ eggs_machina/hw_drivers/transport/usb2can_x2.py
@@ -1,8 +1,16 @@
 """The driver for using the USB2CAN-X2 device by innomaker."""
 
+from dataclasses import dataclass
 from eggs_machina.hw_drivers.transport.base import Transport
 import can
 import os
+import time
+
+@dataclass
+class CAN_Message:
+    can_id: int
+    data_len: int
+    data: bytes
 
 class USB2CANX2(Transport):
     def __init__(self, baud_rate: int):
@@ -15,7 +23,18 @@ class USB2CANX2(Transport):
         
 
     def recv(self, can_id: int, is_extended_id: bool, timeout_s: int, *args, **kwargs) -> any:
-        pass
+        end_time = time.time() + timeout_s
+        while time.time() < end_time:
+            with self.bus as bus:
+                for msg in bus:
+                    if int(msg.arbitration_id) == can_id:
+                        return CAN_Message(
+                            can_id=int(msg.arbitration_id),
+                            data_len=int(len(msg.data)),
+                            data=bytes(msg.data)
+                        )
+        return None
+
 
     def send(self, can_id: int, data: bytes, is_extended_id: bool, *args, **kwargs) -> bool:
         msg = can.Message(arbitration_id=can_id, data=data, is_extended_id=is_extended_id)
