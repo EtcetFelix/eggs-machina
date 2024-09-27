94118380585feca2e35c9361f7da8dec5e7dde3e
Alan Bohannon <alanbohannon@gmail.com>
2024-09-26 17:48:27 -0700

    change script to class

--- eggs_machina/hw_drivers/transport/usb2can_x2.py
+++ eggs_machina/hw_drivers/transport/usb2can_x2.py
@@ -1,46 +1,29 @@
-# """The driver for using the USB2CAN-X2 device by innomaker."""
+"""The driver for using the USB2CAN-X2 device by innomaker."""
 
-# from eggs_machina.hw_drivers.transport.base import Transport
-# import can
-# import os
+from eggs_machina.hw_drivers.transport.base import Transport
+import can
+import os
 
-# class USB2CANX2(Transport):
-#     def __init__(self, baud_rate: int):
-#         self.baud_rate = baud_rate
-#         bus = can.interface.Bus(interface='socketcan', channel='can1', bitrate=baud_rate)
+class USB2CANX2(Transport):
+    def __init__(self, baud_rate: int):
+        os.system('sudo ifconfig can0 down')
+        os.system('sudo ip link set can0 type can bitrate 1000000')
+        os.system("sudo ifconfig can0 txqueuelen 100000")
+        os.system('sudo ifconfig can0 up')
+        self.baud_rate = baud_rate
+        self.bus = can.interface.Bus(channel = 'can0', interface = 'socketcan')
         
 
-#     def recv(self, can_id: int, is_extended_id: bool, timeout_s: int, *args, **kwargs) -> any:
-#         pass
+    def recv(self, can_id: int, is_extended_id: bool, timeout_s: int, *args, **kwargs) -> any:
+        pass
 
-#     def send(self, can_id: int, data: bytes, is_extended_id: bool, *args, **kwargs) -> any:
-#         pass
+    # def send(self, can_id: int, data: bytes, is_extended_id: bool, *args, **kwargs) -> any:
+    def send(self, can_id: int, is_extended_id: bool, *args, **kwargs) -> any:
 
-# if __name__ == "main":
-#     os.system('sudo ifconfig can1 down')
-#     os.system('sudo ip link set can1 type can bitrate 1000000')
-#     os.system("sudo ifconfig can1 txqueuelen 100000")
-#     os.system('sudo ifconfig can1 up')
-#     bus = can.interface.Bus(interface='socketcan', channel='can1', bitrate=20000)
-#     msg = can.Message(arbitration_id=0x123, data=[0,1,2,3,4,5,6,7])
-#     bus.send(msg)
+        msg = can.Message(arbitration_id=0x123, data=[0,1,2,3,4,5,6,7])
+        self.bus.send(msg)
 
-
-import os
-import can
-import time
-
-os.system('sudo ifconfig can0 down')
-os.system('sudo ip link set can0 type can bitrate 1000000')
-os.system("sudo ifconfig can0 txqueuelen 100000")
-os.system('sudo ifconfig can0 up')
-
-can0 = can.interface.Bus(channel = 'can0', interface = 'socketcan')
-send_count = 0
-
-while True:
-    msg = can.Message(arbitration_id=0x123, data=[0,1,2,3,4,5,6,7])
-    can0.send(msg)
-    send_count = send_count + 1
-    print("Currecnt send frame count:", send_count)
-    time.sleep(0.001)
+if __name__ == "__main__":
+    usb2can = USB2CANX2(baud_rate=10000)
+    usb2can.send(1,True)
+    os.system('sudo ifconfig can0 down')
