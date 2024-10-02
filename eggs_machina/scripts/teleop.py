88ab527b09f115e31afa5ece2bcfe96b7435de6b
Alan Bohannon <alanbohannon@gmail.com>
2024-10-02 15:31:57 -0500

    manage robots gracefully

--- eggs_machina/scripts/teleop.py
+++ eggs_machina/scripts/teleop.py
@@ -22,20 +22,20 @@ def shutdown_transport(transport: Transport, can_channel):
     transport.close_channel(can_channel)
 
 
-def instantiate_robots() -> List[Any]:
+def instantiate_robots(transport: Transport) -> List[Any]:
     """Define and instantiate all robots used during teleop."""
-    transport =  can_transport.PCAN(channel=PCANBasic.PCAN_USBBUS2, baud_rate=can_transport.CAN_Baud_Rate.CAN_BAUD_1_MBS)
-    transport2 =  can_transport.PCAN(channel=PCANBasic.PCAN_USBBUS1, baud_rate=can_transport.CAN_Baud_Rate.CAN_BAUD_1_MBS)
-    host_can_id = 150
-    # TODO: set control mode to position
-    # TODO: enable motor
+    # transport =  can_transport.PCAN(channel=PCANBasic.PCAN_USBBUS2, baud_rate=can_transport.CAN_Baud_Rate.CAN_BAUD_1_MBS)
+    # transport2 =  can_transport.PCAN(channel=PCANBasic.PCAN_USBBUS1, baud_rate=can_transport.CAN_Baud_Rate.CAN_BAUD_1_MBS)
+    host_can_id = 0xFD
     leader = Robstride(transport, host_can_id, motor_can_id=127)
     follower = Robstride(transport2, host_can_id, motor_can_id=127)
     follower.write_single_param(Robstride_Param_Enum.RUN_MODE, 1)
     leader.stop_motor()
-    follower.enable_motor()   
+    follower.enable_motor()
+    time.sleep(0.05)   
     set_position(leader, follower)
     time.sleep(1)
+    follower.stop_motor()  
     return [leader, follower]
 
 
@@ -52,23 +52,24 @@ def teleop(leader: Robstride, follower: Robstride):
         time.sleep(0.05)
 
 
-def main():
-    robots = instantiate_robots()
+def main(robots):
     leader = robots[0]
     follower = robots[1]
     start_teleop(leader, follower)
 
 
-
 def shutdown_robots_gracefully(robots: List[Any]):
     """Gracefully turn off all robots."""
+    for robot in robots:
+        robot.stop_motor()
     pass
 
 
 if __name__ == "__main__":
     transport, channel = instantiate_transport()
+    robots = instantiate_robots(transport)
     try:
-        main()
+        main(robots)
     except KeyboardInterrupt:
         print("Shutdown requested...exiting")
         shutdown_robots_gracefully(robots)
@@ -77,3 +78,7 @@ if __name__ == "__main__":
             sys.exit(130)
         except SystemExit:
             os._exit(130)
+    except Exception as e:
+        print("Exception occurred, shutting down transport...")
+        shutdown_transport(transport, channel)
+        print(e)
