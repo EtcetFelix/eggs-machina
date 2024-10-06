174478ef9ae3779a043e25d0a6c3983236a4e8bd
Alan Bohannon <alanbohannon@gmail.com>
2024-10-05 22:41:57 -0500

    don't use pytest fixture for now since can needing sudo

--- tests/integration/test_robstride_actuator.py
+++ tests/integration/test_robstride_actuator.py
@@ -2,20 +2,38 @@ import pytest
 from eggs_machina.hw_drivers.transport.can.usb2can_x2 import USB2CANX2
 from eggs_machina.hw_drivers.system.robstride.robstride import Robstride
 from eggs_machina.hw_drivers.system.robstride.robstride_types import Robstride_Param_Enum
-
+import time
 
 
 CAN_CHANNEL = "can0"
 MOTOR_ID = 127
 
-@pytest.fixture
-def robstride_transport():
+def test_get_vbus():
     with USB2CANX2(channel=CAN_CHANNEL, baud_rate=1000000) as transport:
         robstride = Robstride(host_can_id=0xFD, motor_can_id=MOTOR_ID, can_transport=transport)
-        yield robstride  # Provide the robstride instance to the test
+        bus_voltage = robstride.read_single_param(Robstride_Param_Enum.VBUS_VOLTAGE)
+        print(bus_voltage)
+        assert bus_voltage is not None, "Failed to read bus_voltage. Check can channel, power, transport device, or device id."
 
-def test_get_vbus(robstride_transport): 
-    bus_voltage = robstride_transport.read_single_param(Robstride_Param_Enum.VBUS_VOLTAGE)
-    print(bus_voltage)
-    assert bus_voltage is not None, "Failed to read bus_voltage..."
 
+def test_read_run_mode():
+    with USB2CANX2(channel=CAN_CHANNEL, baud_rate=1000000) as transport:
+        robstride = Robstride(host_can_id=0xFD, motor_can_id=MOTOR_ID, can_transport=transport)
+        control_mode = robstride.read_single_param(Robstride_Param_Enum.RUN_MODE)
+        print(control_mode)
+
+def test_position_control(robstride: Robstride):
+        robstride.write_single_param(Robstride_Param_Enum.RUN_MODE, 1)
+        robstride.enable_motor()
+        robstride.write_single_param(Robstride_Param_Enum.POSITION_MODE_ANGLE_CMD, 2)
+        feedback = robstride.get_motor_feedback_frame()
+        print(feedback)
+        time.sleep(1)
+        robstride.stop_motor()
+        robstride.write_single_param(Robstride_Param_Enum.RUN_MODE, 0)
+        pos = robstride.read_single_param(Robstride_Param_Enum.MECH_POS_END_COIL)
+        print(pos)
+
+if __name__ == "__main__":
+    test_get_vbus()
+    test_read_run_mode()
